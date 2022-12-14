4576333c4a14dc3a160f1eeabe70f4739e43eb0c
Yaroslav <yaroslav.s@yozma.tech>
2022-12-14 09:42:12 +0200

    remove main.ts config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { config } from 'dotenv';
-
-config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
